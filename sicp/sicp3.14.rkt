bec8a19a1b438cf5a436e2863c6e4f9b681ea1e1
unknown <seanm@.tnb.com>
2011-12-01 18:13:23 -0600

    Through 3.14

---
+++ sicp/sicp3.14.rkt
@@ -0,0 +1,16 @@
+;Exercise 3.14.  The following procedure is quite useful, although obscure:
+
+(define (mystery x)
+  (define (loop x y)
+    (if (null? x)
+        y
+        (let ((temp (cdr x)))
+          (set-cdr! x y)
+          (loop temp x))))
+  (loop x '()))
+
+;Loop uses the ``temporary'' variable temp to hold the old value of the cdr of x, since the set-cdr!
+;on the next line destroys the cdr. Explain what mystery does in general. Suppose v is defined by 
+;(define v (list 'a 'b 'c 'd)). Draw the box-and-pointer diagram that represents the list to which v is bound. Suppose that we now evaluate (define w (mystery v)). Draw box-and-pointer diagrams that show the structures v and w after evaluating this expression. What would be printed as the values of v and w ?
+
+;This procedure basically reverses a list, leaving a list with just the first original element in its place. At the end of calling (mystery v), the reversed list is returned.  If we check the value of v, it should be a list with the first value only.
