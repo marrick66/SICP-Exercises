9507930bb4d658956832ea61f094c3922691aa81
Sean Mayfield <marrick66@gmail.com>
2012-02-07 16:12:24 -0600

    Through 3.45

---
+++ sicp/sicp3.45.rkt
@@ -0,0 +1,30 @@
+;Exercise 3.45.  Louis Reasoner thinks our bank-account system is unnecessarily complex and error-prone now that deposits and withdrawals aren't automatically serialized. He suggests that make-account-and-serializer should have exported the serializer (for use by such procedures as serialized-exchange) in addition to (rather than instead of) using it to serialize accounts and deposits as make-account did. He proposes to redefine accounts as follows:
+
+(define (make-account-and-serializer balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (let ((balance-serializer (make-serializer)))
+    (define (dispatch m)
+      (cond ((eq? m 'withdraw) (balance-serializer withdraw))
+            ((eq? m 'deposit) (balance-serializer deposit))
+            ((eq? m 'balance) balance)
+            ((eq? m 'serializer) balance-serializer)
+            (else (error "Unknown request -- MAKE-ACCOUNT"
+                         m))))
+    dispatch))
+
+;Then deposits are handled as with the original make-account:
+
+(define (deposit account amount)
+ ((account 'deposit) amount))
+
+;Explain what is wrong with Louis's reasoning. In particular, consider what happens when serialized-exchange is called.
+
+
+;Since serialized-exchanged takes both serializers and wraps the exchange call on them, each serializer for the accounts is called twice.  It's unclear, but since the serializer is already called once, the second call will be deadlocked.
